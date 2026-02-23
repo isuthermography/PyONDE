c47bced1e35c04aa6286ede28f94bc71f9b3077e
JudsonM03 <judson09@iastate.edu>
2026-02-23 14:28:46 -0600

    Added kwargs for ONDEValue constructor

--- PyONDE/build_simple_graph.py
+++ PyONDE/build_simple_graph.py
@@ -3,7 +3,7 @@ import onde
 
 leaf = onde.ONDEValue.new("leaf value", _frozen = True)
 leaf2 = onde.ONDEValue.new("leaf value 2", _frozen = True)
-obj = onde.ONDEObject.new(leaf = leaf)
+obj = onde.ONDEObject.new(_ONDE_type = ["myobject"], leaf = leaf)
 obj.leaf2 = leaf2
 obj.leaf_secondreference = leaf
 obj.freeze()
